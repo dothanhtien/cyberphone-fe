1eba5de50245a8d41e5660938c309f3109156b89
Tien Do Thanh <dothanhtien95@gmail.com>
2025-09-02 17:08:59 +0700

    add redux persist

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { useRef } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { makeStore, AppStore } from "@/lib/store";
+import { persistor, store } from "@/lib/store";
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore | null>(null);
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
-
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,14 +1,50 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 import authReducer from "./features/auth/authSlice";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      auth: authReducer,
-    },
-  });
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.NEXT_PUBLIC_REDUX_ENCRYPTION_KEY || "",
+      onError: (error) =>
+        console.error("Redux persist encryption error:", error),
+    }),
+  ],
+  version: 1, // Add versioning to prevent migration issues
 };
 
-export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+const persistedReducer = persistReducer<RootState>(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type AppStore = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
