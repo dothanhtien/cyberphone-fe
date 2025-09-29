3bdec972df0675fb729c59ed4b48ea207c8b2eed
Tien Do Thanh <dothanhtien95@gmail.com>
2025-09-30 00:28:25 +0700

    add admin layout

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,23 +1,3 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
-
 export default function Dashboard() {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    localStorage.removeItem("accessToken");
-    router.push("/admin/login");
-  };
-
-  return (
-    <div className="p-8">
-      <h1>Dashboard</h1>
-      <div className="mt-4">
-        <Button onClick={handleLogout}>Log out</Button>
-      </div>
-    </div>
-  );
+  return <h1>Dashboard</h1>;
 }
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,25 @@
 import type { Metadata } from "next";
 
+import "../globals.css";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { AppSidebar } from "@/components/layouts";
+import { Separator } from "@/components/ui/separator";
+import { Toaster } from "@/components/ui/sonner";
 import ProtectedRoute from "@/components/protectedRoute";
 
 export const metadata: Metadata = {
-  title: "CyberPhone | Admin",
-  description: "To be defined",
+  title: "Cyberphone | Admin",
+  description: "TBD",
 };
 
 export default function AdminLayout({
@@ -12,5 +27,40 @@ export default function AdminLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
+  return (
+    <ProtectedRoute>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="min-h-[100vh] flex-1 rounded-xl md:min-h-min p-8">
+              {children}
+            </div>
+          </div>
+
+          <Toaster position="top-right" richColors />
+        </SidebarInset>
+      </SidebarProvider>
+    </ProtectedRoute>
+  );
 }
